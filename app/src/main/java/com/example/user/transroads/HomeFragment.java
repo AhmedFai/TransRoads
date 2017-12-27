08ed7137b820d9c4d7f5ee43a6f9a90e3a452972
Mukul <mukulraw199517@gmail.com>
2017-12-27 17:05:54 +0530

    transroads

--- app/src/main/java/com/example/user/transroads/HomeFragment.java
+++ app/src/main/java/com/example/user/transroads/HomeFragment.java
@@ -4,34 +4,40 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
 
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView homeRecycler;
+    homeCardAdapter adapter;
+    GridLayoutManager manager;
 
-    public static HomeFragment newInstance() {
-        HomeFragment fragment = new HomeFragment();
-        return fragment;
-        // Required empty public constructor
-    }
+    ImageView image;
 
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        homeRecycler = (RecyclerView) view.findViewById(R.id.homeRecycler);
+        manager = new GridLayoutManager(getContext(),1);
+        adapter = new homeCardAdapter(getContext());
+        homeRecycler.setLayoutManager(manager);
+        homeRecycler.setAdapter(adapter);
+
+        image = (ImageView) view.findViewById(R.id.profileImage);
+
+
+
+        return view;
     }
 
 }
