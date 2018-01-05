61d1459c13d6d3d2a9de658f421ddad152c76a54
Mukul <mukulraw199517@gmail.com>
2018-01-05 19:13:26 +0530

    transroads

--- app/src/main/java/com/example/user/transroads/FavoriteSpot.java
+++ app/src/main/java/com/example/user/transroads/FavoriteSpot.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,14 +15,23 @@ import android.view.ViewGroup;
 public class FavoriteSpot extends Fragment {
 
 
+    RecyclerView favoriteRecycle;
+    ReviewCardAdapter adapter;
+    GridLayoutManager manager;
 
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_spot, container, false);
+       View view = inflater.inflate(R.layout.fragment_favorite_spot,container,false);
+
+       favoriteRecycle = (RecyclerView) view.findViewById(R.id.favoriteRecycler);
+       manager = new GridLayoutManager(getContext(), 2);
+       adapter = new ReviewCardAdapter(getContext());
+       favoriteRecycle.setLayoutManager(manager);
+       favoriteRecycle.setAdapter(adapter);
+       return view;
     }
 
 
