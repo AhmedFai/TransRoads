e0dd12fcd9cbb626b98ab4dd273bbe1576d60829
Faizan <faizan.fz2085@gmail.com>
2018-07-15 21:56:36 +0530

    Fused location and screen shot

---
+++ app/src/main/java/com/example/user/transroads/AddNewPost.java
@@ -0,0 +1,24 @@
+package com.example.user.transroads;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class AddNewPost extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_post);
+         imageView = findViewById(R.id.ss);
+
+        byte[] byteArray = getIntent().getByteArrayExtra("image");
+        Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        imageView.setImageBitmap(bmp);
+    }
+}
