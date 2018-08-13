aef5d0452b5e1f7cf136b878b6ac1016f96f04e7
AhmedFai <faizan.fz2085@gmail.com>
2018-08-13 17:42:59 +0530

    Five Tabs

--- app/src/main/java/com/example/user/transroads/AddNewPost.java
+++ app/src/main/java/com/example/user/transroads/AddNewPost.java
@@ -1,19 +1,26 @@
 package com.example.user.transroads;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
 public class AddNewPost extends AppCompatActivity  implements View.OnClickListener{
 
     private ImageView imageView;
     private Bitmap bitmap;
     TextView cancel;
-    ImageView bus, motor, car,bike,train,plane;
+    ImageView bus, motor, car,bike,train,plane, addImage;
+    private static  final int GALLERY_REQUEST=1;
+    private Uri mImageUri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,17 @@ public class AddNewPost extends AppCompatActivity  implements View.OnClickListen
          train.setOnClickListener(this);
          bike.setOnClickListener(this);
          plane.setOnClickListener(this);
+         addImage = (ImageView)findViewById(R.id.addImage);
+
+         addImage.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent galleryIntent = new Intent(
+                         Intent.ACTION_PICK,
+                         android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                 startActivityForResult(galleryIntent ,GALLERY_REQUEST );
+             }
+         });
 
 
          cancel.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +64,31 @@ public class AddNewPost extends AppCompatActivity  implements View.OnClickListen
         byte[] byteArray = getIntent().getByteArrayExtra("image");
         Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
         imageView.setImageBitmap(bmp);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+
+            Uri imageUri = data.getData();
+
+            CropImage.activity(imageUri)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+                mImageUri = result.getUri();
+                addImage.setImageURI(mImageUri);
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 
     @Override
